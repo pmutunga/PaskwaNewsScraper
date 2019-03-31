f65af4a990579e12442489f768cb836f696d058e
pmutunga <paskwa.mutunga@ipsafari.com>
2019-03-30 22:19:58 -0400

    comments front-end

--- public/assets/javascript/index.js
+++ public/assets/javascript/index.js
@@ -71,36 +71,62 @@ $.getJSON("/headlines", function(data) {
   // });
     }); 
 
-// Whenever someone clicks a p tag
-$(document).on("click", "p", function() {
-  // Empty the notes from the note section
-  $("#notes").empty();
-  // Save the id from the p tag
+// // Whenever someone clicks the add comments button
+$(document).on("click", ".addcomment", function() {
+  console.log("saving comments front end initiated");
+// Save the id from the p tag
+var thisId = $(this).attr("data-id");
+var commentdiv = "#" + thisId;
+console.log(commentdiv);
+
+$(commentdiv).removeClass("hide");
+
+// Now make an ajax call for the Article
+$.ajax({
+  method: "GET",
+  url: "/headlines/" + thisId
+})
+  // With that done, add the note information to the page
+  .then(function(data) {
+    console.log(data);
+
+    var saveNote = "#savenote" + thisId;
+    console.log(saveNote);
+
+// When you click the savenote button
+$(document).on("click", saveNote, function() {
+
+  console.log("submit comment front end initiated")
+  // Grab the id associated with the article from the submit button
   var thisId = $(this).attr("data-id");
+  var bodyInput = "#comment" + thisId;
+  console.log(bodyInput);
 
-  // Now make an ajax call for the Article
+  // Run a POST request to change the note, using what's entered in the inputs
   $.ajax({
-    method: "GET",
-    url: "/articles/" + thisId
+    method: "POST",
+    url: "/headlines/" + thisId,
+    data: {
+      // Value taken from note textarea
+      body: $(bodyInput).val()
+    }
   })
-    // With that done, add the note information to the page
+    // With that done
     .then(function(data) {
+      // Log the response
       console.log(data);
-      // The title of the article
-      $("#notes").append("<h2>" + data.title + "</h2>");
-      // An input to enter a new title
-      $("#notes").append("<input id='titleinput' name='title' >");
-      // A textarea to add a new note body
-      $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
-      // A button to submit a new note, with the id of the article saved to it
-      $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
-
-      // If there's a note in the article
-      if (data.note) {
-        // Place the title of the note in the title input
-        $("#titleinput").val(data.note.title);
-        // Place the body of the note in the body textarea
-        $("#bodyinput").val(data.note.body);
-      }
+      // Empty the notes section
+      $(bodyInput).empty();
     });
+
+  // Also, remove the values entered in the input and textarea for note entry
+  // $("#titleinput").val("");
+  $("#bodyinput").val("");
 });
+
+
+ });
+
+
+});
+
--- server.js
+++ server.js
@@ -158,6 +158,7 @@ app.post("/headlines/:id", function(req, res) {
   // Create a new note and pass the req.body to the entry
   db.Note.create(req.body)
     .then(function(dbNote) {
+      console.log("save comment back end initiated");
       // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
       // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
       // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
