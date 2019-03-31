3838de45eb334d43e39af0c6bcbe6ad2d9ab726e
pmutunga <paskwa.mutunga@ipsafari.com>
2019-03-31 00:10:54 -0400

    save notes backend

--- public/assets/javascript/index.js
+++ public/assets/javascript/index.js
@@ -1,132 +1,139 @@
 // Grab the articles as a json
 $.getJSON("/headlines", function(data) {
-    // For each one
-    for (var i = 0; i < data.length; i++) {
-      // Display the apropos information on the page
-      $("#scrapedarticles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
-    }
-  });
-  
-  $.getJSON("/saved", function(data) {
-    // For each one
-    for (var i = 0; i < data.length; i++) {
-      // Display the apropos information on the page
-      $("#scrapedarticles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
-    }
-  });
+  // For each one
+  for (var i = 0; i < data.length; i++) {
+    // Display the apropos information on the page
+    $("#scrapedarticles").append(
+      "<p data-id='" +
+        data[i]._id +
+        "'>" +
+        data[i].title +
+        "<br />" +
+        data[i].link +
+        "</p>"
+    );
+  }
+});
 
+$.getJSON("/saved", function(data) {
+  // For each one
+  for (var i = 0; i < data.length; i++) {
+    // Display the apropos information on the page
+    $("#scrapedarticles").append(
+      "<p data-id='" +
+        data[i]._id +
+        "'>" +
+        data[i].title +
+        "<br />" +
+        data[i].link +
+        "</p>"
+    );
+  }
+});
 
-
-    
-  //When you click the save button
-  $(document).on("click", ".save", function() {
-    console.log('Client-side code running');
-    // Grab the id associated with the article from the submit button
-    var thisId = $(this).attr("data-id");
-  
-    // Run a POST request to save the value of saved from false to true.
-    $.ajax({
-      method: "POST",
-      url: "/saveheadlines/" + thisId
-      // data: {
-      //   // change value of saved to true
-      //   saved: true,
-      // }
-    })
-      // With that done
-      .then(function(data) {
-        // Log the response
-        console.log("Client-side code finished running");
-        // res.redirect("/headlines");
-        $(".save").addClass(".hide");
-        });
-
-  // });
-    }); 
-
-    //When you click the remove-fav button
-  $(document).on("click", ".remove-fav", function() {
-    console.log('Client-side code running');
-    // Grab the id associated with the article from the submit button
-    var thisId = $(this).attr("data-id");
-  
-    // Run a POST request to save the value of saved from false to true.
-    $.ajax({
-      method: "POST",
-      url: "/unsaveheadlines/" + thisId
-      // data: {
-      //   // change value of saved to true
-      //   saved: true,
-      // }
-    })
-      // With that done
-      .then(function(data) {
-        // Log the response
-        console.log("Client-side code finished running");
-        // res.redirect("/headlines");
-        // $("#results-modal").modal("toggle");
-        $(".save").removeClass(".hide");
-      });
-
-  // });
-    }); 
-
-// // Whenever someone clicks the add comments button
-$(document).on("click", ".addcomment", function() {
-  console.log("saving comments front end initiated");
-// Save the id from the p tag
-var thisId = $(this).attr("data-id");
-var commentdiv = "#" + thisId;
-console.log(commentdiv);
-
-$(commentdiv).removeClass("hide");
-
-// Now make an ajax call for the Article
-$.ajax({
-  method: "GET",
-  url: "/headlines/" + thisId
-})
-  // With that done, add the note information to the page
-  .then(function(data) {
-    console.log(data);
-
-    var saveNote = "#savenote" + thisId;
-    console.log(saveNote);
-
-// When you click the savenote button
-$(document).on("click", saveNote, function() {
-
-  console.log("submit comment front end initiated")
+//When you click the save button
+$(document).on("click", ".save", function() {
+  console.log("Client-side code running");
   // Grab the id associated with the article from the submit button
   var thisId = $(this).attr("data-id");
-  var bodyInput = "#comment" + thisId;
-  console.log(bodyInput);
 
-  // Run a POST request to change the note, using what's entered in the inputs
+  // Run a POST request to save the value of saved from false to true.
   $.ajax({
     method: "POST",
-    url: "/headlines/" + thisId,
-    data: {
-      // Value taken from note textarea
-      body: $(bodyInput).val()
-    }
+    url: "/saveheadlines/" + thisId
+    // data: {
+    //   // change value of saved to true
+    //   saved: true,
+    // }
   })
     // With that done
     .then(function(data) {
       // Log the response
-      console.log(data);
-      // Empty the notes section
-      $(bodyInput).empty();
+      console.log("Client-side code finished running");
+      // res.redirect("/headlines");
+      $(".save").addClass(".hide");
     });
 
-  // Also, remove the values entered in the input and textarea for note entry
-  // $("#titleinput").val("");
-  $("#bodyinput").val("");
+  // });
 });
 
+//When you click the remove-fav button
+$(document).on("click", ".remove-fav", function() {
+  console.log("Client-side code running");
+  // Grab the id associated with the article from the submit button
+  var thisId = $(this).attr("data-id");
 
- });
+  // Run a POST request to save the value of saved from false to true.
+  $.ajax({
+    method: "POST",
+    url: "/unsaveheadlines/" + thisId
+    // data: {
+    //   // change value of saved to true
+    //   saved: true,
+    // }
+  })
+    // With that done
+    .then(function(data) {
+      // Log the response
+      console.log("Client-side code finished running");
+      // res.redirect("/headlines");
+      // $("#results-modal").modal("toggle");
+      $(".save").removeClass(".hide");
+    });
 
-
+  // });
 });
 
+// // Whenever someone clicks the add comments button
+$(document).on("click", ".addcomment", function() {
+  console.log("saving comments front end initiated");
+  // Save the id from the p tag
+  var thisId = $(this).attr("data-id");
+  var commentdiv = "#" + thisId;
+  console.log(commentdiv);
+
+  $(commentdiv).removeClass("hide");
+
+  // Now make an ajax call for the Article
+  $.ajax({
+    method: "GET",
+    url: "/headlines/" + thisId
+  })
+    // With that done, add the note information to the page
+    .then(function(data) {
+      console.log(data);
+
+      var saveNote = "#savenote" + thisId;
+      console.log(saveNote);
+
+      // When you click the savenote button
+      $(document).on("click", saveNote, function() {
+        console.log("submit comment front end initiated");
+        // Grab the id associated with the article from the submit button
+        var thisId = $(this).attr("data-id");
+        var bodyInput = "#comment" + thisId;
+        console.log(bodyInput);
+
+        // Run a POST request to change the note, using what's entered in the inputs
+        $.ajax({
+          method: "POST",
+          url: "/headlines/" + thisId,
+          data: {
+            // Value taken from note textarea
+            body: $(bodyInput).val()
+          }
+        })
+          // With that done
+          .then(function(data) {
+            // Log the response
+            console.log(data);
+            // Empty the notes section
+            $(bodyInput).empty();
+          });
+
+        // Also, remove the values entered in the input and textarea for note entry
+        // $("#titleinput").val("");
+        $("#bodyinput").val("");
+      });
+    });
+});
--- server.js
+++ server.js
@@ -137,18 +137,34 @@ app.get("/saved", function(req, res) {
 });
 
 
+
 // Route for grabbing a specific Article by id, populate it with it's note
 app.get("/headlines/:id", function(req, res) {
   // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
   db.Headline.findOne({ _id: req.params.id })
     // ..and populate all of the notes associated with it
     .populate("note")
-    .then(function(dbHeadline) {
+    .then(function(dbArticle) {
       // If we were able to successfully find an Article with the given id, send it back to the client
-      res.json(dbHeadline);
+      res.json(dbArticle);
     })
     .catch(function(err) {
       // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+
+// Route for retrieving all Notes from the db
+app.get("/notes", function(req, res) {
+  // Find all Notes
+  db.Note.find({})
+    .then(function(dbNote) {
+      // If all Notes are successfully found, send them back to the client
+      res.json(dbNote);
+    })
+    .catch(function(err) {
+      // If an error occurs, send the error back to the client
       res.json(err);
     });
 });
@@ -158,25 +174,22 @@ app.post("/headlines/:id", function(req, res) {
   // Create a new note and pass the req.body to the entry
   db.Note.create(req.body)
     .then(function(dbNote) {
-      console.log("save comment back end initiated");
       // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
       // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
       // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-      return db.HeadLine.findOneAndUpdate(
-        { _id: req.params.id },
-        { note: dbNote._id },
-        { new: true }
-      );
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
     })
-    .then(function(dbHeadline) {
+    .then(function(dbArticle) {
       // If we were able to successfully update an Article, send it back to the client
-      res.json(dbHeadline);
+      res.json(dbArticle);
     })
     .catch(function(err) {
       // If an error occurred, send it to the client
       res.json(err);
     });
 });
+
+
 
 //route for saving article
 app.post("/saveheadlines/:id", function(req, res) {
